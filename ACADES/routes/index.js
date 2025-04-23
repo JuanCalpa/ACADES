f27f0c39358ec28b3ded9cbbac62c1ede20fea14
DESKTOP-M8H8U09\Usuario <jcalpalopez@gmail.com>
2025-04-22 20:22:47 -0500

    implementacion de cors

--- ACADES/routes/index.js
+++ ACADES/routes/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const app = express();
-const rutasUsuarios = require('./routes/rutas');
 const cors = require('cors');
 
-app.use(cors()); // permite todas las peticiones desde cualquier origen
+const app = express();
 
+//configurar cors
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
+//configurar body parsing
 app.use(express.json());
+
+//importar y usar rutas
+const rutasUsuarios = require('./routes/rutas');
 app.use('/ACADES/usuarios', rutasUsuarios);
 
+//iniciar el servidor
 app.listen(3000, () => {
   console.log('Servidor escuchando en http://localhost:3000');
 });
-
-
