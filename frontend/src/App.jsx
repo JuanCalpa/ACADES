20b1f6da4dceb62b6fba284c2e6ce1a810ef482f
sofi burbano <141693771+ingesofi123@users.noreply.github.com>
2025-05-19 21:56:34 -0500

    penultimooo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Login from './components/Login';
 import AgendaCita from './components/AgendaCita';
+import Header from './Header'; // Asegúrate de importar tu Header
 import './App.css';
+
+function AppContent() {
+  const location = useLocation();
+  // Rutas header no
+  const hideHeaderRoutes = ['/Especialista', '/Admin', '/Registro', '/perfilD'];
+  const shouldHideHeader = hideHeaderRoutes.some(route =>
+    location.pathname.startsWith(route)
+  );
+
+
+  return (
+    <>
+      {!shouldHideHeader && <Header />}
+      <Routes>
+        <Route path="/Especialista" element={<EspecialistaPanel />} />
+        <Route path="/Admin" element={<AdminPanel />} />
+        <Route path="/Registro" element={<Registro />} />
+        <Route path="/perfilD" element={<PerfilD />} />
+      </Routes>
+    </>
+  );
+}
+
+
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/agenda-cita" element={<AgendaCita />} />
-        {}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <AppContent />
     </Router>
   );
 }
